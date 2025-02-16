f7a1b6e9751406f1f12aa3368bc2699a167839b2
Ashish <ashishvason002@gmail.com>
2025-02-16 23:07:23 +0530

    playing with the next middleware

--- src/app.js
+++ src/app.js
@@ -10,19 +10,28 @@ app.get('/user', (req, res) =>
   })
 );
 
-// regex
-
-app.get('/ab?c', (req, res) => res.send('playing with routes')); // b is optional
-app.get('/ab+c', (req, res) => res.send('playing with routes')); // b is as many as possible.
-app.get('/ab*c', (req, res) => res.send('playing with routes mul'));
-app.get('([A-Z])w+', (req, res) => res.send('playing with regex')); // regex
-
-app.post('/user', (req, res) => res.send('user data saved Successfully.'));
-app.patch('/user', (req, res) => res.send('user data updated Successfully.'));
-app.delete('/user', (req, res) => res.send('user data deleted Successfully.'));
-
-app.use('/test', (req, res) => res.send('Test port'));
-app.use('/hello', (req, res) => res.send('Hello hello hellooo!'));
+app.use(
+  '/test',
+  (req, res, next) => {
+    // res.send('Response!!');
+    next();
+  },
+  (req, res, next) => {
+    console.log('route handler 2');
+    // res.send('Response 2nd');
+    next();
+  },
+  (req, res, next) => {
+    console.log('route handler 3');
+    // res.send('Response 3rd');
+    next();
+  },
+  (req, res, next) => {
+    console.log('route handler 4');
+    res.send('Response 4th');
+    // next();
+  }
+);
 // app.use('/', (req, res) => res.send('Hello from the Home Page'));
 
 app.listen(port, () => console.log('Server is listening at port', port));
