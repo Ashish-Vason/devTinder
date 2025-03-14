239a34dbb04b70377bf163c3542b94bbf4f16a3f
Ashish <ashishvason002@gmail.com>
2025-03-14 16:57:27 +0530

    added data sanitization and validations

--- src/app.js
+++ src/app.js
@@ -6,21 +6,81 @@ const User = require('./models/User');
 const app = express();
 const port = 7777;
 
+app.use(express.json());
+
+app.get(
+  '/userfeed',
+  async (req, res) => {
+    try {
+      const users = await User.find({});
+      res.send(users);
+    } catch (error) {
+      res.status(400).send('Something went wrong!!!');
+    }
+  }
+  // const users = await User.find({});
+  // res.send(users);
+);
+
+app.get('/userbyemail', async (req, res) => {
+  try {
+    const users = await User.find({ email: req.body.email });
+    if (!users || users.length < 1) {
+      res.status(404).send('User not found in db!');
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send('Something went wrong!!!');
+  }
+});
+
+app.delete('/user', async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    await User.findByIdAndDelete(userId);
+    res.send('User deleted successfully!!!');
+  } catch (error) {
+    res.status(400).send('Something went wrong!!');
+  }
+});
+
+app.patch('/user/:userId', async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const data = req.body;
+    const user = await User.findByIdAndUpdate(userId, data, {
+      returnDocument: 'before',
+      runValidators: true,
+    });
+    const ALLOWED_UPDATES = ['skills', 'about', 'photoURL', 'password', 'age'];
+    const isAllowedUpdates = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+
+    if (data.skills && data?.skills.length > 10) {
+      throw new Error('skills should not be exceeded beyond 10!!');
+    }
+
+    if (!isAllowedUpdates) {
+      throw new Error('Update not allowed for these fields!!');
+    }
+
+    res.send('User updated successfully for' + user);
+  } catch (error) {
+    res.status(400).send('Update Failed!!' + error);
+  }
+});
+
 app.post('/signup', async (req, res) => {
-  const userData = {
-    firstName: 'Ashish',
-    lastName: 'Vason',
-    email: 'ashish@gmail.com',
-    password: 'abc123',
-  };
   // creating new instance of userModal.
-  const user = new User(userData);
+  const user = new User(req.body);
   try {
     await user.save();
     // to save in the database
     res.send('User Added Successfully!!');
   } catch (err) {
-    res.status(400).send('Error in saving user!:', err.message);
+    res.status(400).send('Error in saving user!:' + err.message);
   }
 });
 
--- src/models/User.js
+++ src/models/User.js
@@ -1,23 +1,89 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 200,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      minLength: 4,
+      maxLength: 200,
+      trim: true,
+      minLength: 4,
+      maxLength: 200,
+    }, // String is shorthand for {type: String}
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      minLength: 4,
+      maxLength: 200,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error('Please enter a valid email', value);
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 20,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error('Please enter a strong password', value);
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+      maxLength: 4,
+    },
+    gender: {
+      type: String,
+      minLength: 4,
+      maxLength: 20,
+      validate(value) {
+        if (!['male', 'female', 'others'].includes(value)) {
+          throw new Error('Invalid Gender');
+        }
+      },
+    },
+    photoURL: {
+      type: String,
+      minLength: 4,
+      maxLength: 200,
+      default:
+        'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png',
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error('The photoURL is not valid', value);
+        }
+      },
+    },
+
+    about: {
+      type: String,
+      default: 'About Section of the application user',
+      minLength: 4,
+      maxLength: 200,
+    },
+    skills: {
+      type: [String],
+      maxLength: 300,
+    },
   },
-  lastName: String, // String is shorthand for {type: String}
-  email: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 // Modal is like a class where we can structure the userSchema.
 
